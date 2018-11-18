31737ce0227bd66c9160206b8f132ab434db4147
Weixin Li <40030861+Weixin8999@users.noreply.github.com>
2018-11-18 13:51:15 -0800

    Add files via upload

---
+++ Betting_Code/BettingYearsCombined.py
@@ -0,0 +1,63 @@
+"""
+This program scrapes NBA matches' data from a betting odds website
+and saves all the years from 2009 to 2018 to a single file.
+"""
+
+# Imports
+from bs4 import BeautifulSoup
+from selenium import webdriver
+import time
+
+# Setting up Selenium browser
+chromepath = r"C:\Users\c1yeung\Desktop\chromedriver.exe"
+options = webdriver.ChromeOptions()
+options.add_argument('headless')
+browser = webdriver.Chrome(chromepath, chrome_options=options)
+
+# Setting up URLs to iterate
+baseURL_left = "https://www.oddsportal.com/basketball/usa/nba-"
+baseURL_right = "/results/#/page/"
+URL = {}
+for year in range(2008, 2018):
+    URL[year] = baseURL_left + str(year) + "-" + str(int(year) + 1) + baseURL_right
+
+# Scraping the data
+finalodds = []
+for year in URL:
+    print("Now scraping the", str(year) + " to " + str(year + 1) + " NBA season")
+    for page in range(1, 30):
+        pageURL = URL[year] + str(page)
+        browser.get(pageURL)
+        time.sleep(2)
+        source = browser.page_source
+        soup = BeautifulSoup(source, 'lxml')
+        odds_table = soup.find('table', {'class': 'table-main'})
+        full_table = odds_table.find('tbody').findAll('tr', {'class': 'deactivate'})
+        temp = []
+
+        # Adding data to table
+        for table in full_table:
+            teamNames = table.find('td', {'class': 'table-participant'}).find('a').get_text()
+            temp.append(teamNames)
+            odds = table.findAll('td', {'class': 'odds-nowrp'})
+            for odd in odds:
+                gameOdds = odd.contents[0].get_text()
+                temp.append(gameOdds)
+            finalodds.append(temp)
+            temp = []
+# Close browser
+browser.close()
+
+# Formatting the data
+formatted_odds = [[]]
+for game in finalodds:
+    teams = game[0].split(" - ")
+    formatted_game = [teams[0], teams[1], game[1], game[2]]
+    formatted_odds.append(formatted_game)
+formatted_odds.remove([])
+
+# Exporting the data into a csv file
+with open("bettingOdds.csv", "w") as csv:
+    for game in formatted_odds:
+        csv.write(str(game[0]) + "," + str(game[1]) + "," + str(game[2]) + "," + str(game[3]))
+        csv.write("\n")
---
+++ Player_Code/StatsYearsSeparated.py
@@ -0,0 +1,52 @@
+"""
+This program is used to scrape NBA data and separate into a different
+file for each year between 1989 and 2018 for all NBA players (30 years).
+"""
+
+# Imports
+from bs4 import BeautifulSoup
+import requests
+import csv
+
+# This part creates a dictionary of URLs for each year from 1989 to 2018.
+baseURL_left = "https://www.basketball-reference.com/leagues/NBA_"
+baseURL_right = "_advanced.html"
+URL = {}
+for year in range(1989, 2019):
+    URL[year] = baseURL_left + str(year) + baseURL_right
+
+# This part scrapes data from the NBA database and creates a table for all stats
+for year in URL:
+    print("Getting stats from " + str(year))
+    source = requests.get(URL[year]).text
+    soup = BeautifulSoup(source, 'lxml')
+    player_table = soup.find('table', {'class': 'sortable stats_table'})
+    full_table = player_table.find('tbody').findAll('tr', {'class': 'full_table'})
+    total_table = []
+    for table in full_table:
+        player_name = table.find('td', {'data-stat': "player"}).get_text()
+        stats = table.findAll('td', {'class': 'right'})
+        team = table.find('td', {'data-stat': "team_id"}).get_text()
+        temp = [player_name, team]
+
+        # Cleaning up empty columns and adding data to overall table
+        column = 0
+        for stat in stats:
+            column += 1
+            if column == 16 or column == 21:
+                continue
+            playerstats = stat.get_text()
+            if not playerstats:
+                playerstats = "0"
+                temp.append(playerstats)
+            else:
+                temp.append(playerstats)
+        total_table.append(temp)
+
+    # Exporting data to csv file
+    filename = "AvgPlayerStats" + str(year) + ".csv"
+    with open(filename, 'w', newline='') as csvfile:
+        csvwriter = csv.writer(csvfile)
+        csvwriter.writerows(total_table)
+    print("Done")
+print("All tasks complete")
