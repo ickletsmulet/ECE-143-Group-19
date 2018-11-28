d5d5cd7ea47965563430c2c8eb8e4cf6fe207ef3
yidzhu9 <yidi6848@163.com>
2018-11-27 21:20:37 -0800

    visual

--- preprocessing/data_norm.py
+++ preprocessing/data_norm.py
@@ -1,5 +1,6 @@
 def data_norm(data_t):
     import numpy as np
+    import math
     # do data normaliazation to put all data in range of [0,1]
     minu = data_t[:,0].reshape([-1,1])
     # the contribution of a player is decided by his time on game
@@ -7,9 +8,20 @@ def data_norm(data_t):
     for i in range(data_t.shape[1]):
         min_col = np.min(data_t[:,i])
         max_col = np.max(data_t[:,i])
+        mean_col = np.mean(data_t[:,i])
+        var_col = np.var(data_t[:,i])
         for j in range(data_t.shape[0]):
+            #uniform
+            #data_t[j,i] = float(data_t[j,i] - min_col)/(max_col- min_col)
+            #normal distribution
+            data_t[j,i] = float(data_t[j,i] - mean_col)/math.sqrt(var_col)
+        
+        min_col = np.min(data_t[:,i])
+        max_col = np.max(data_t[:,i])
+        for j in range(data_t.shape[0]):
+            #uniform
             data_t[j,i] = float(data_t[j,i] - min_col)/(max_col- min_col)
-
+            #data_t[j,i]/=16
     #need_idx = [1,2,3,4,5,6,7,8,9,10,11,12,17,18,19,20]
     #data_te = data_t[:,need_idx]
     return data_t
