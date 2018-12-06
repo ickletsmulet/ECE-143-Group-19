7dcbcbb367d3fcea0bbe966f9caf1b957e5ec7d8
yidzhu9 <yidi6848@163.com>
2018-12-05 22:36:26 -0800

    modify

--- preprocessing/data_norm.py
+++ preprocessing/data_norm.py
@@ -1,4 +1,12 @@
 def data_norm(data_t):
+    '''
+    this module is to do data normaliazation(first turn to normal distribution and then put them into range[0,1])
+    
+    input:data_t
+    type: numpy.array() in which the first column is the minutes one player played total season
+    output:data_t
+    type: numpy.array() in which all the data is normalized without the first column
+    '''
     import numpy as np
     import math
     # do data normaliazation to put all data in range of [0,1]
@@ -6,13 +14,11 @@ def data_norm(data_t):
     # the contribution of a player is decided by his time on game
     data_t *=minu
     for i in range(data_t.shape[1]):
-        min_col = np.min(data_t[:,i])
-        max_col = np.max(data_t[:,i])
+
         mean_col = np.mean(data_t[:,i])
         var_col = np.var(data_t[:,i])
         for j in range(data_t.shape[0]):
-            #uniform
-            #data_t[j,i] = float(data_t[j,i] - min_col)/(max_col- min_col)
+            
             #normal distribution
             data_t[j,i] = float(data_t[j,i] - mean_col)/math.sqrt(var_col)
         
@@ -21,7 +27,5 @@ def data_norm(data_t):
         for j in range(data_t.shape[0]):
             #uniform
             data_t[j,i] = float(data_t[j,i] - min_col)/(max_col- min_col)
-            #data_t[j,i]/=16
-    #need_idx = [1,2,3,4,5,6,7,8,9,10,11,12,17,18,19,20]
-    #data_te = data_t[:,need_idx]
+
     return data_t
